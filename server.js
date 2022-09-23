6e7baeb5dc77efca7d83a666e71e0ee77bba2816
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-09-23 10:56:11 -0300

    Add Express:Session and Passport Middleware

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const passport = require('passport');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -11,6 +13,15 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize())
 
 app.route('/').get((req, res) => {
    // Change the response to render the Pug template
