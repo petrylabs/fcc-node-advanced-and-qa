b833cd8005fe26e4084d9650a7dcaaba6a89da36
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-09-23 11:16:20 -0300

    Add Passport Serialize and Deserialize Functions

--- server.js
+++ server.js
@@ -24,6 +24,17 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  //   done(null, null);
+  // });
+  done(null, null);
+});
+
 app.route('/').get((req, res) => {
    // Change the response to render the Pug template
    res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
