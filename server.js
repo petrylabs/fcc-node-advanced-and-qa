2db9acb504c2af196f0a5bdeb86210cea3c363d0
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-09-23 11:40:49 -0300

    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -25,21 +25,42 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+    res.render(process.cwd() + '/views/pug/index', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+    
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+    
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, null);
+    });
+
+  });
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
 });
 
-passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-  //   done(null, null);
-  // });
-  done(null, null);
-});
 
-app.route('/').get((req, res) => {
-   // Change the response to render the Pug template
-   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
-});
+
+// app.route('/').get((req, res) => {
+//    // Change the response to render the Pug template
+//    res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
