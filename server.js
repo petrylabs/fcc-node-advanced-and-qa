f99ff575626167d7179bbb67f0734d96cb78c2dc
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-09-23 09:44:36 -0300

    Set view engine to pug

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+app.set('view engine', 'pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,7 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+   // Change the response to render the Pug template
+   res.render(process.cwd() + '/views/pug');
 });
 
 const PORT = process.env.PORT || 3000;
