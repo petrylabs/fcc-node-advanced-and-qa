01410f0f3f603b87c841c1afa3bf7015e8419d1a
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-09-23 15:43:22 -0300

    Require passport local and tell passport to use instantiated local strategy object"

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -24,6 +25,19 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+//  Tell passport to use an instantiated LocalStrategy object with a few settings defined
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    myDataBase.findOne({ username: username }, function (err, user) {
+      console.log('User '+ username +' attempted to log in.');
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
