832853c628ef41d6b3750f81cd5e5890c98341ec
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-09-26 22:13:42 -0300

    Updates

--- routes.js
+++ routes.js
@@ -1,87 +1,59 @@
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 
-module.exports = function(app, myDataBase) {
-    
-    app.route('/').get((req, res) => {
-        res.render(process.cwd() + '/views/pug/index', {
-          title: 'Connected to Database',
-          message: 'Please login',
-          showLogin: true,
-          showRegistration: true,
-          showSocialAuth: true
-        });
-      });
-    
-    app.route('/login')
-      .post(passport.authenticate('local' ,{ failureRedirect: '/' }), function(req, res) {
-       res.redirect('/profile');
-    });
-
-    app.route('/profile').get(ensureAuthenticated, function(req, res) {
-        // Pass an object containing the property username and value of req.user.username as the second argument for the render method of the profile view
-        res.render(process.cwd() + '/views/pug/profile', {
-          username: req.user.username
-        });
-    });
-
-    app.route('/logout')
-    .get((req, res) => {
-        req.logout();
-        res.redirect('/');
-    });
-  
-    app.route('/register')
-    .post((req, res, next) => {
-      myDataBase.findOne({ username: req.body.username }, function(err, user) {
+module.exports = function (app, myDataBase) {
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('pug', { title: 'Connected to Database', message: 'Please login', showLogin: true, showRegistration: true, showSocialAuth: true });
+  });
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  });
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    res.render('pug/profile', { username: req.user.username });
+  });
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+  app.route('/register').post(
+    (req, res, next) => {
+      const hash = bcrypt.hashSync(req.body.password, 12);
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
         if (err) {
           next(err);
         } else if (user) {
           res.redirect('/');
         } else {
-          // hash password
-          const pwdHash = bcrypt.hashSync(req.body.password, 12);
-          myDataBase.insertOne({
-            username: req.body.username,
-            password: pwdHash
-          },
-            (err, doc) => {
-              if (err) {
-                res.redirect('/');
-              } else {
-                // The inserted document is held within
-                // the ops property of the doc
-                next(null, doc.ops[0]);
-              }
+          myDataBase.insertOne({ username: req.body.username, password: hash }, (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
             }
-          )
+          });
         }
-      })
-    },
-      passport.authenticate('local', { failureRedirect: '/' }),
-      (req, res, next) => {
-        res.redirect('/profile');
-      }
-    );
-    
-    app.route('/auth/github')
-      .get(passport.authenticate('github'));
-
-    app.route('/auth/github/callback')
-      .get(passport.authenticate('github', { failureRedirect: '/' }), (req,res) => {
-          res.redirect('/profile');
-        });
-
-    app.use((req, res, next) => {
-        res.status(404)
-          .type('text')
-          .send('Not Found');
       });
-}
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
+};
 
 function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect('/');
-};
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
