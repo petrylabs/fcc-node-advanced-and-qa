25bbefa5d6c61f76d3f3180e3cad5685cb1e08b0
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-09-26 21:47:14 -0300

    Fix github routes
    Routes were calling passport.authenticate in the route handler and not as a middleware

--- routes.js
+++ routes.js
@@ -65,16 +65,12 @@ module.exports = function(app, myDataBase) {
     );
     
     app.route('/auth/github')
-      .get((req, res) => {
-        passport.authenticate('github')
-      });
+      .get(passport.authenticate('github'));
 
     app.route('/auth/github/callback')
-      .get((req, res) => {
-        passport.authenticate('github', { failureRedirect: '/' }), (req,res) => {
+      .get(passport.authenticate('github', { failureRedirect: '/' }), (req,res) => {
           res.redirect('/profile');
-        }
-      });
+        });
 
     app.use((req, res, next) => {
         res.status(404)
