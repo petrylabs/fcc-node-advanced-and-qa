92cfa338f1520a3d4b1cfa8a53f12439481e8ef1
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-09-30 14:07:04 -0300

    Authenticate with Socket.IO

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const myDB = require('./connection');
@@ -10,9 +11,13 @@ const routes = require('./routes.js');
 const auth = require('./auth.js');
 
 const app = express();
-
 const http = require('http').createServer(app);
 const io = require('socket.io')(http)
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({url: URI});
 
 app.use(cors());
 app.set('view engine', 'pug');
@@ -25,13 +30,37 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
+  key: 'express.sid',
   resave: true,
   saveUninitialized: true,
+  store: store,
   cookie: { secure: false }
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+)
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
@@ -42,7 +71,7 @@ myDB(async client => {
   io.on('connection', (socket) => {
     ++currentUsers;
     console.log('A user has connected!, Connected Users:', currentUsers);
-    
+    console.log('user ' + socket.request.user.name + ' connected');
     io.emit('user count', currentUsers);
     socket.on('disconnect', () => {
       --currentUsers;
